7f9509ea77333c65bce3e8c38ef605888e1d0785
kremol <kremol@163.com>
2017-05-29 10:50:25 +0800

    solved thread problem

--- app/src/main/java/com/kremol/market4/MainActivity.java
+++ app/src/main/java/com/kremol/market4/MainActivity.java
@@ -1,6 +1,8 @@
 package com.kremol.market4;
 
 import android.content.Intent;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -40,15 +42,19 @@ import static com.kremol.market4.R.layout.entry;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-
+    public static final int INIT_PRODUCT_OK = 0;
 
     //五种商品类别
-    enum TYPE{NEWTHING,ELECTRONICS,CLOTHES,BOOK,OHTER,};
+    enum TYPE {
+        NEWTHING, ELECTRONICS, CLOTHES, BOOK, OHTER,
+    }
+
+    ;
 
     //一个适配器
     private EntryAdapter entryAdapter;
 
-    TextView newthingText,electronicsText,bookText,clothesText,otherText;
+    TextView newthingText, electronicsText, bookText, clothesText, otherText;
     ListView listView;
     ImageView headImg;
 
@@ -56,7 +62,28 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     int typeNow = 0;   //当前选择的类别
     List<Product> productList;      //所有商品
-    List<List<Entry>>   entryLists;     //5个entryList
+    List<List<Entry>> entryLists;     //5个entryList
+
+    /*handler*/
+    private Handler handler = new Handler() {
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case INIT_PRODUCT_OK:
+                      /*将product转成entry并分成5个类别，最终得到entryLists*/
+                    List<Entry> entryList = changeToEntryList(productList);
+                    entryLists = classifyEntry(entryList);
+
+                     /*初始时用"typeNow(最新")构造adpater并设置*/
+                    entryAdapter = new EntryAdapter(MainActivity.this, R.layout.entry, entryLists.get(typeNow));
+                    listView.setAdapter(entryAdapter);
+                    break;
+
+                default:
+                    break;
+            }
+        }
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -68,13 +95,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         user = (User) getIntent().getSerializableExtra("user");
 
         /*得到控件*/
-            listView = (ListView) findViewById(R.id.list_view);
-//        newthingText = (TextView) findViewById(R.id.new_thing);
-//        electronicsText = (TextView) findViewById(R.id.electronics);
-//        bookText = (TextView) findViewById(R.id.book);
-//        clothesText = (TextView) findViewById(R.id.clothes);
-//        otherText = (TextView) findViewById(R.id.other);
-            headImg = (ImageView) findViewById(R.id.head_information);
+        listView = (ListView) findViewById(R.id.list_view);
+        headImg = (ImageView) findViewById(R.id.head_information);
 
 
         /*设置头像*/
@@ -84,8 +106,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         headImg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this,UserActivity.class);
-                intent.putExtra("user",user);
+                Intent intent = new Intent(MainActivity.this, UserActivity.class);
+                intent.putExtra("user", user);
                 startActivity(intent);
             }
         });
@@ -95,26 +117,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Entry entry = entryLists.get(typeNow).get(position);
-                Intent intent = new Intent(MainActivity.this,ProductActivity.class);
-                intent.putExtra("product",entry.getProduct());
+                Intent intent = new Intent(MainActivity.this, ProductActivity.class);
+                intent.putExtra("product", entry.getProduct());
                 startActivity(intent);
             }
         });
 
-        /*将product转成entry并分成5个类别，最终得到entryLists*/
+        /*初始化productList*/
         initProduct();
-        List<Entry> entryList  = changeToEntryList(productList);
-        entryLists = classifyEntry(entryList);
 
-        /*初始时用"typeNow(最新")构造adpater并设置*/
-        entryAdapter = new EntryAdapter(MainActivity.this,R.layout.entry,entryLists.get(typeNow));
-        listView.setAdapter(entryAdapter);
 
     }
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
+        switch (v.getId()) {
             /*重新设置adpater*/
             case R.id.new_thing:
                 typeNow = 0;
@@ -134,16 +151,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             default:
                 break;
         }
-        entryAdapter = new EntryAdapter(MainActivity.this,R.layout.entry,entryLists.get(typeNow));
+        entryAdapter = new EntryAdapter(MainActivity.this, R.layout.entry, entryLists.get(typeNow));
         listView.setAdapter(entryAdapter);
     }
 
 
-
     //初始化商品
-    public void initProduct(){
+    public void initProduct() {
         try {
-           String address = "http://localhost:8088/secondary/ GetAllProductsServlet";
+            String address = "http://localhost:8088/secondary/ GetAllProductsServlet";
             HttpUtil.sendHttpRequest(address, new okhttp3.Callback() {
                 @Override
                 public void onFailure(Call call, IOException e) {
@@ -155,7 +171,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     String responseData = response.body().string();
                     Gson gson = new Gson();
                     //获得productList
-                    productList = gson.fromJson(responseData,new TypeToken<List<Product>>(){}.getType());
+                    productList = gson.fromJson(responseData, new TypeToken<List<Product>>() {
+                    }.getType());
+                    Message message = new Message();
+                    message.what = INIT_PRODUCT_OK;
+                    handler.sendMessage(message);
                 }
             });
         } catch (Exception e) {
@@ -165,46 +185,41 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 
     //将product转成entryList
-    public List<Entry> changeToEntryList(List<Product> productList){
-        List<Entry>entryList = new ArrayList<>();
-        for(int i=0;i<productList.size();i++){
+    public List<Entry> changeToEntryList(List<Product> productList) {
+        List<Entry> entryList = new ArrayList<>();
+        for (int i = 0; i < productList.size(); i++) {
             Product product = productList.get(i);
-            String nickName  = product.getUserforsale();
+            String nickName = product.getUserforsale();
             String productName = product.getTitle();
 
             //第一个应该用商品user的head
-            Entry entry = new Entry(0,nickName,product,productName);
+            Entry entry = new Entry(0, nickName, product, productName);
             entryList.add(entry);
         }
         return entryList;
     }
 
     //把entryList分类成5个entryList
-    public List<List<Entry>> classifyEntry(List<Entry> entryList){
+    public List<List<Entry>> classifyEntry(List<Entry> entryList) {
         List<List<Entry>> entryLists = new ArrayList<>(5);
         int group = -1;
-        for(int i=0;i<entryList.size();i++){
+        for (int i = 0; i < entryList.size(); i++) {
             Entry entry = entryList.get(i);
             String type = entry.getProduct().getType();
-            if( type.equals("最新")){
+            if (type.equals("最新")) {
                 group = 0;
-            }
-            else if(type.equals("电子")){
+            } else if (type.equals("电子")) {
                 group = 1;
-            }
-            else if(type.equals("书籍")){
+            } else if (type.equals("书籍")) {
                 group = 2;
-            }
-            else if(type.equals("衣物")){
+            } else if (type.equals("衣物")) {
                 group = 3;
-            }
-            else if(type.equals("其他")){
+            } else if (type.equals("其他")) {
                 group = 4;
-            }
-            else {
+            } else {
                 group = -1;
             }
-            if(group>=0 && group<=4)
+            if (group >= 0 && group <= 4)
                 entryLists.get(group).add(entry);
         }
 
